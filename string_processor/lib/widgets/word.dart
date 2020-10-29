07acd901a3befe6d65f4117c02499eca6ae1bfa3
Ivy <ivyla3@gmail.com>
2020-10-28 18:07:35 -0700

    button sizing is how i want it

--- string_processor/lib/widgets/word.dart
+++ string_processor/lib/widgets/word.dart
@@ -3,15 +3,18 @@ import 'message.dart';
 
 class Word extends StatefulWidget {
   final word;
+
   Word(this.word);
 
   @override
-  _WordState createState() => _WordState(word);
+  _WordState createState() => _WordState(word, 0);
 }
 
 class _WordState extends State<Word> {
   final String buttonLabel;
-  _WordState(this.buttonLabel);
+  int clicks;
+  var defaultFontSize = 25.0;
+  _WordState(this.buttonLabel, this.clicks);
 
   String getButtonLabel() {
     return buttonLabel;
@@ -28,14 +31,28 @@ class _WordState extends State<Word> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FlatButton(
+      child: RawMaterialButton(
         // padding: EdgeInsets.all(0),
         materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-        child: Text(buttonLabel, style: TextStyle(fontSize: 25.0)),
+        child: Text(buttonLabel, style: TextStyle(fontSize: defaultFontSize)),
         onPressed: () => handlePress(),
-        color: Colors.blue,
-        textColor: Colors.black,
+        fillColor: Colors.blue,
+        constraints:
+            BoxConstraints(minWidth: buttonLabel.length.toDouble() * 14),
+        // textColor: Colors.black,
       ),
     );
   }
 }
+// Widget build(BuildContext context) {
+//   return Container(
+//     child: FlatButton(
+//       // padding: EdgeInsets.all(0),
+//       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+//       child: Text(buttonLabel, style: TextStyle(fontSize: 25.0)),
+//       onPressed: () => handlePress(),
+//       color: Colors.blue,
+//       textColor: Colors.black,
+//     ),
+//   );
+// }
